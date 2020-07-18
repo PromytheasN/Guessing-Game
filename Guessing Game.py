d8a2f9d975f77f471ad32b96944b1d8f4cbc75be
PromytheasN <63561890+PromytheasN@users.noreply.github.com>
2020-07-17 18:45:46 -0700

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,11 +1,29 @@
 import random 
 
 def rand_num():
+    """
+    Function that picks a random number between 0, 100
+    """
     return random.randint(0,100)
     
 def user_guess_input():
+    """
+    Function that asks for an integer input from the user
+    """
     guess =""
     while True:
         guess = input("Please input your guess number! ")
         if guess.isnumeric():
             return int(guess)
+
+def comp_feed_back(user_guess, pc_choice):
+    """
+    Function that checks if users input match pc's choice, and gives a feed back if not
+    """
+    if user_guess == pc_choice:
+        found = True
+        return "You found the number!! Congrats!!"
+    elif user_guess < pc_choice:
+        return "The number is larger than the one you guessed!"
+    else:
+        return "The number is smaller than the one you guessed!"
